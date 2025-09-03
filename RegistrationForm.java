48b75ff560f91f1f4360e01050b530f2391cb12b
Vinay Daripelly <152810364+Vinay-Daripelly@users.noreply.github.com>
2025-09-03 10:23:51 +0530

    Create  RegistrationForm.java

---
+++ RegistrationForm.java
@@ -0,0 +1,6 @@
+public class RegistrationForm {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Event Registration System!");
+        System.out.println("Registration Successful!");
+    }
+}
